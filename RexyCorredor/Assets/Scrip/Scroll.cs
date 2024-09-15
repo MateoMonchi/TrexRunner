462b32b9ab3b17012bc64c7b5881c6898f0675db
MateoMonchi <monchiettimateo1@gmail.com>
2024-09-15 20:39:38 -0300

    Fead:piso infinito

---
+++ RexyCorredor/Assets/Scrip/Scroll.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+    private float scrollSpeed = 8f;
+    void Update()
+    {
+       transform.Translate(Vector2.left *  scrollSpeed * Time.deltaTime); 
+    }
+}
